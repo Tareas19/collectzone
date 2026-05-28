7f8735a796150b7102df95ed73d96ed52233e976
Angel <jfjmyz5t56@privaterelay.appleid.com>
2026-05-28 13:00:29 +0200

    fix lazy loading carrito

--- collectzone-backend/collectzone/src/main/java/com/collectzone/repository/CarritoRepository.java
+++ collectzone-backend/collectzone/src/main/java/com/collectzone/repository/CarritoRepository.java
@@ -1,12 +1,12 @@
 package com.collectzone.repository;
-
 import com.collectzone.model.Carrito;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
 @Repository
 public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+    @EntityGraph(attributePaths = {"usuario", "articulo"})
     List<Carrito> findByUsuarioId(Long usuarioId);
     boolean existsByUsuarioIdAndArticuloId(Long usuarioId, Long articuloId);
     void deleteByUsuarioIdAndArticuloId(Long usuarioId, Long articuloId);
