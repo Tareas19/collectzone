0be09d81e4b22d3a3e80aa417573a9b77cce75c4
Angel <jfjmyz5t56@privaterelay.appleid.com>
2026-05-28 12:43:24 +0200

    fix lazy loading with EntityGraph

--- collectzone-backend/collectzone/src/main/java/com/collectzone/repository/FavoritoRepository.java
+++ collectzone-backend/collectzone/src/main/java/com/collectzone/repository/FavoritoRepository.java
@@ -1,12 +1,12 @@
 package com.collectzone.repository;
-
 import com.collectzone.model.Favorito;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
 @Repository
 public interface FavoritoRepository extends JpaRepository<Favorito, Long> {
+    @EntityGraph(attributePaths = {"usuario", "articulo"})
     List<Favorito> findByUsuarioId(Long usuarioId);
     boolean existsByUsuarioIdAndArticuloId(Long usuarioId, Long articuloId);
     void deleteByUsuarioIdAndArticuloId(Long usuarioId, Long articuloId);
